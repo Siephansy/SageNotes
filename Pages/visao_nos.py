7dfa1ed3540c211a43e827cfbd7d8ce03fb40350
Phelipe Braga <49958979+Siephansy@users.noreply.github.com>
2025-03-06 17:15:10 -0300

    21
    21

--- Pages/visao_nos.py
+++ Pages/visao_nos.py
@@ -8,8 +8,8 @@ st.title("Visão de Nós")
 # Lógica para construir e exibir a visão de nós
 # (Leia os dados do Google Sheets e use networkx)
 # Exemplo simples (substitua pela sua lógica real):
-G = nx.Graph()
-G.add_edges_from([(1, 2), (1, 3), (2, 4), (3, 4)])
-pos = nx.spring_layout(G)
-nx.draw(G, pos, with_labels=True)
-st.pyplot(plt)
+#G = nx.Graph()
+#G.add_edges_from([(1, 2), (1, 3), (2, 4), (3, 4)])
+#pos = nx.spring_layout(G)
+#nx.draw(G, pos, with_labels=True)
+#st.pyplot(plt)
