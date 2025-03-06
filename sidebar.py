1cb3185d01405972fe41bbd6346ba11b41119ac2
Phelipe Braga <49958979+Siephansy@users.noreply.github.com>
2025-03-06 16:19:41 -0300

    5
    5

--- sidebar.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def sidebar_content():
-    st.sidebar.header("Navegação")
-    pagina_selecionada = st.sidebar.radio("Selecione uma página", ["Envio de Arquivos", "Visão de Nós", "Anotações"])
-    return pagina_selecionada
