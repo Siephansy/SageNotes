4242be00d650e0e025057c3697d1c9c098ed6c35
Phelipe Braga <phelipebc@terra.com.br>
2025-03-08 03:13:46 -0300

    29
    29

--- pages/Visão de Nós.py
+++ pages/Visão de Nós.py
@@ -4,16 +4,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-st.title("Visão de Nós")
-# Lógica para construir e exibir a visão de nós
-# (Leia os dados do Google Sheets e use networkx)
-# Exemplo simples (substitua pela sua lógica real):
-#G = nx.Graph()
-#G.add_edges_from([(1, 2), (1, 3), (2, 4), (3, 4)])
-#pos = nx.spring_layout(G)
-#nx.draw(G, pos, with_labels=True)
-#st.pyplot(plt)
-
 
 st.header("Visão de Nós")
 # Lógica para construir e exibir a visão de nós
