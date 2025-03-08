2d211fdd858cf8c9c5e68d1676be62cd7c1abc3a
Phelipe Braga <phelipebc@terra.com.br>
2025-03-08 02:17:53 -0300

    28
    28

--- pages/Anotações.py
+++ pages/Anotações.py
@@ -6,19 +6,6 @@ import matplotlib.pyplot as plt
 
 
 
-
-st.header("Visão de Nós")
-# Lógica para construir e exibir a visão de nós
-# (Leia os dados do Google Sheets e use networkx)
-# Exemplo simples (substitua pela sua lógica real):
-G = nx.Graph()
-G.add_edges_from([(1, 2), (1, 3), (2, 4), (3, 4)])
-pos = nx.spring_layout(G)
-nx.draw(G, pos, with_labels=True)
-st.pyplot(plt)
-
-
-
 st.header("Anotações")
 # Lógica para exibir as anotações
 # (Leia os dados do Google Sheets e exiba em uma tabela ou lista)
--- pages/Visão de Nós.py
+++ pages/Visão de Nós.py
@@ -13,3 +13,15 @@ st.title("Visão de Nós")
 #pos = nx.spring_layout(G)
 #nx.draw(G, pos, with_labels=True)
 #st.pyplot(plt)
+
+
+st.header("Visão de Nós")
+# Lógica para construir e exibir a visão de nós
+# (Leia os dados do Google Sheets e use networkx)
+# Exemplo simples (substitua pela sua lógica real):
+G = nx.Graph()
+G.add_edges_from([(1, 2), (1, 3), (2, 4), (3, 4)])
+pos = nx.spring_layout(G)
+nx.draw(G, pos, with_labels=True)
+st.pyplot(plt)
+
