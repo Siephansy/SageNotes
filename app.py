ad033dc4e24e11f78a0854e82803f77ed1bfd6f8
Phelipe Braga <phelipebc@terra.com.br>
2025-03-08 04:09:18 -0300

    35
    35

--- app.py
+++ app.py
@@ -5,6 +5,18 @@ import requests
 from datetime import datetime 
 
 
+st.subheader("Webhook Client")
+url = st.text_input("Webhook URL", "https://webhook-server.com/webhook-1")
+payload = st.text_input("JSON Payload", "{}")
+
+if st.button("Submit"):
+    if not url.strip() == "":
+        response = requests.post(url, data = payload)
+        st.success(f"Response: {response.text}")
+    else:
+        st.error(f"Response: Please provide the Webhook URL.")
+
+
 # Função para enviar arquivo para o n8n
 def enviar_arquivo_n8n(arquivo):
     # Configurar a URL do webhook do n8n
