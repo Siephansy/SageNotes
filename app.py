9d29e59ef028652b0c5693a641a13c8a1a44b287
Phelipe Braga <phelipebc@terra.com.br>
2025-03-08 04:13:45 -0300

    38
    38

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ payload = st.text_input("JSON Payload", "{}")
 
 if st.button("Submit"):
     if not url.strip() == "":
+        st.subheader("a")
         response = requests.post(url, data = payload)
         st.success(f"Response: {response.text}")
     else:
+        st.subheader("b")
         st.error(f"Response: Please provide the Webhook URL.")
 
 
@@ -22,22 +24,32 @@ def enviar_arquivo_n8n(arquivo):
     # Configurar a URL do webhook do n8n
     webhook_url = "http://127.0.0.1:5678/webhook-test/7109995a-fbfa-4b8a-8049-8ee62622d853"  # Substitua pela sua URL do webhook
 
+    st.subheader("c")
+
     # Enviar arquivo para o n8n
     try:
+        st.subheader("d")
         files = {"arquivo": (arquivo.name, arquivo, arquivo.type)}  # Formato correto para enviar arquivos com requests
         response = requests.post(webhook_url, files=files)
+        st.subheader("e")
 
         # Verificar a resposta do n8n
         if response.status_code == 200:
+            st.subheader("f")
             st.success("Arquivo enviado com sucesso para o n8n!")
             print("Arquivo enviado com sucesso para o n8n!")
+            st.subheader("g")
         else:
+            st.subheader("h)
             st.error(f"Erro ao enviar arquivo para o n8n: {response.status_code}")
             print(f"Erro ao enviar arquivo para o n8n: {response.status_code}")
             print(response.text) # Print the error returned by n8n
+            st.subheader("i")
     except Exception as e:
+        st.subheader("j")
         st.error(f"Erro ao enviar o arquivo: {e}")
         print(f"Erro ao enviar o arquivo: {e}")
+        st.subheader("k")
 
 
 
